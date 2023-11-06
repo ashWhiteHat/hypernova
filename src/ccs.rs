841b0eb793375f92c19896766919e7cc3080ad43
ashWhiteHat <phantomofrotten@gmail.com>
2023-11-06 12:32:58 +0900

    css is sat

--- src/ccs.rs
+++ src/ccs.rs
@@ -24,31 +24,22 @@ pub struct Ccs<F: PrimeField, const t: usize, const q: usize> {
 
 impl<F: PrimeField, const t: usize, const q: usize> Ccs<F, t, q> {
     pub(crate) fn is_sat(&self) -> bool {
-        let vectors: Vec<DenseVectors<F>> = (0..q)
-            .map(|i| {
-                // constant scalar of matrix
-                let c = self.constants[i];
-                // matrix indexes
-                let s = self.multisets[i].clone();
-                let j_vectors: Vec<DenseVectors<F>> = s
-                    .iter()
-                    .map(|j| {
-                        let matrix = self.matrices[i].clone();
-                        matrix.prod(self.m, &self.x, &self.w)
-                    })
-                    .collect();
-                let mut acc = DenseVectors::identity(self.m);
-                for vector in j_vectors {
-                    acc = acc * vector
-                }
-                acc * c
-            })
-            .collect();
-        let mut identity = DenseVectors(vec![F::zero(); self.m]);
-        for vector in vectors {
-            identity = identity + vector
-        }
-        identity.iter().all(|vector| vector == F::zero())
+        // compute sum of q vector
+        let mut final_vector = DenseVectors(vec![F::zero(); self.m]);
+        (0..q).for_each(|i| {
+            let mut product = DenseVectors::identity(self.m);
+            self.multisets[i].iter().for_each(|j| {
+                // matrix vector multiplication
+                let vector = self.matrices[*j].prod(self.m, &self.x, &self.w);
+                // vector hadamard product
+                product = product.clone() * vector;
+            });
+            // constant scalar of matrix
+            let c = self.constants[i];
+            let q_vector = product * c;
+            final_vector = final_vector.clone() + q_vector;
+        });
+        final_vector.iter().all(|vector| vector == F::zero())
     }
 }
 
